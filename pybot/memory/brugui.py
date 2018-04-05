5c8a712daae35af90c8ca2d7d596d8e6abc1e120
Alejandro Brugarolas <alejandro_brugarolas@hotmail.com>
2018-04-05 13:17:03 +0200

    Created brugui_utils

--- pybot/memory/brugui.py
+++ pybot/memory/brugui.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 """
- rael.py        Pesonal stuff.
- Author:        Rael Garcia <self@rael.io>
- Date:          06/2016
- Tested on:     Python 3 / OS X 10.11.5
+ brugui.py      Pesonal stuff.
+ @author:       bruguii
+ @since:        05/2018
 """
 import re
 import sys
 
 
-def rael_utils(words):
+def brugui_utils(words):
     "Function to metion me when my name appears in a message"
 
-    if re.search(r'(^|\s)rael(\s|$)', words, re.I | re.M):
-        return "@raelga"
+    if re.search(r'(^|\s)brugui(\s|$)', words, re.I | re.M):
+        return "@bruguii"
 
 
 def hear(words):
     "Implements hear to receive the messages and execute the plugin logic"
-    return rael_utils(words)
+    return brugui_utils(words)
 
 
 def main(argv):
